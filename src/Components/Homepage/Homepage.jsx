1f4c3fb06dffd82e350db003aab70ac30d24d630
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-04-09 13:57:27 +0600

    Hero section is created

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import bookImage from "../../assets/hero_img.jpg"
 const Homepage = () => {
   return (
     <div>
-      <h1 className='text-2xl text-center'>HomePage</h1>
+      <div className="hero bg-base-200 min-h-[70vh]  w-11/12 mx-auto mt-7 rounded-2xl">
+  <div className="hero-content flex flex-col justify-between lg:flex-row-reverse w-full">
+    <img
+      src={bookImage}
+      className="max-w-sm rounded-lg shadow-2xl"
+    />
+    <div className='space-y-6'>
+      <h1 className="text-6xl font-bold">Books to freshen up <br /> your bookshelf</h1>
+      <button className="btn bg-[#23BE0A] rounded-lg text-white">Get Started</button>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
